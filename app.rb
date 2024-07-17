21021e0734872ebc7339d3a70c5830c3df7b6054
Rocky Latchman <kidmongoose@gmail.com>
2024-07-16 23:50:34 -0700

    Sign in form

--- app.rb
+++ app.rb
@@ -7,5 +7,41 @@ class LemurApp < Sinatra::Base
     erb :index
   end
 
+  get '/ticket-tracker' do
+  
+  end
+
+  get '/ticket-tracker/item/:item_id' do
+    #get ticket item
+  end
+
+  get '/ticket-tracker/add/project' do
+     #get project
+  end
+  
+  post '/ticket-tracker/add/project' do
+    #add project
+  end
+
+  post '/requirement/:item_id' do
+    #add requirement
+  end  
+
+  post '/problem/:requirement_id' do
+     #add problem for requirement
+  end
+  
+
+  get '/progress-tracker' do
+   #get progress tracker
+  end  
+
+  get '/progress-tracker/chart/:item_id' do
+   #get gantt chart for item
+  end
+
+
+
+
   run!
 end
